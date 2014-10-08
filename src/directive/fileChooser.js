81f5aef3b20b45de82defac903c454dd146b8e41
Jorin Vogel <jorin.vogel@gmail.com>
2014-10-08 20:29:08 +0700

    on osx use text input for private key config field. fixes #1

--- src/directive/fileChooser.js
+++ src/directive/fileChooser.js
@@ -1,11 +1,21 @@
 app.directive('fileChooser', function() {
+
+  var macTemplate = '<input type="text" value="{{path}}" class="form-control" placeholder="Path to file">';
+  var normalTemplate = '<a class="btn btn-default">{{path || "choose a file"}}</a><input type="file">';
+  var isMac = process.platform === 'darwin';
+  
   return {
     restrict: 'E',
     scope: {
       path: '='
     },
-    template: '<a class="btn btn-default">{{path || "choose a file"}}</a><input type="file">',
+    template: isMac ? macTemplate : normalTemplate,
     link: function(scope, el) {
+      if (isMac) {
+        scope.path = '~/.ssh/id_rsa';
+        return;
+      }
+
       var fileInput = el.find('input');
       el.find('a').bind('click', fileInput[0].click.bind(fileInput[0]));
 
